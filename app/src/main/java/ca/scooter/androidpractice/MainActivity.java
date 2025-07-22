112f3a9fb0def7e017393def606d306896f62b50
Scooter <92922224+scooterthedev@users.noreply.github.com>
2025-07-22 13:53:49 -0400

    removed fragment, and got setup_screen started

--- app/src/main/java/ca/scooter/androidpractice/MainActivity.java
+++ app/src/main/java/ca/scooter/androidpractice/MainActivity.java
@@ -28,28 +28,19 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-
         Intent intent = getIntent();
         boolean needsAuth = intent.getBooleanExtra("NEEDS_AUTH", false);
         boolean isFirstInMain = intent.getBooleanExtra("IS_FIRST_TIME", false);
 
         if (needsAuth || mAuth.getCurrentUser() == null) {
-
-            NavOptions navOptions = new NavOptions.Builder()
-                    .setPopUpTo(navController.getGraph().getStartDestinationId(), true)
-                    .build();
-            navController.navigate(R.id.loginActivity, null, navOptions);
-
             Intent authIntent = new Intent(this, LoginActivity.class);
             startActivity(authIntent);
             finish();
         } else if (isFirstInMain) {
             // THis is for when the user is authed, but firt time opening like when they want to restart onboarding on re-install the app without clearing the data
-            NavOptions navOptions = new NavOptions.Builder()
-                    .setPopUpTo(navController.getGraph().getStartDestinationId(), true)
-                    .build();
-            navController.navigate(R.id.setup_page, null, navOptions);
+            Intent setupIntent = new Intent(this, setup_page.class);
+            startActivity(setupIntent);
+            finish();
             }
         }
 
--- app/src/main/java/ca/scooter/androidpractice/setup_page.java
+++ app/src/main/java/ca/scooter/androidpractice/setup_page.java
@@ -2,51 +2,23 @@ package ca.scooter.androidpractice;
 
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import ca.scooter.androidpractice.databinding.ActivitySetupPageBinding;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class setup_page extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivitySetupPageBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivitySetupPageBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_setup_page);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_setup_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_setup_page);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
